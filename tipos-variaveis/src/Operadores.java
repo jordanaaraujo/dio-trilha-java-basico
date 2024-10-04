ed7fb7dd23435e76f34626f9307d00c0b5d2000c
jordanaaraujo <jordanaa_araujo@hotmail.com>
2024-10-04 09:28:16 -0300

    Estudo(operadores)

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 12;
+        b = 6;
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+            System.out.println(resultado);
+
+}
+
+
+}
+
